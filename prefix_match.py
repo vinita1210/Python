d1b1c40693df6693fe4899543242ea6caff3f7a5
vinita1210 <62983088+vinita1210@users.noreply.github.com>
2021-01-02 19:24:33 +0530

    Update prefix_match.py

--- prefix_match.py
+++ prefix_match.py
@@ -1,32 +1,42 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        #print(strs)
+        #print(len(strs))
         output=''
         value=''
         if len(strs) != 0:
             min_len=len(strs[0])
+        if len(strs) == 1:
+            output = strs[0]
+            print(output)
         #print(min_len)
-        for i in strs:
-            if (len(i) < min_len):
-                min_len = len(i)
-                value = i
-        #print(value)        
-        #print(min_len)
-        main_str = [x for x in strs if x != value] 
-        #print(main_str)
-        #print("value of length : " + str(len(main_str)) )
-        if value != '':
-            for i in range(min_len):
-                #print(i)
-                c=0
-                val = value[i]
-                for x in main_str:
-                    if val in x[i]:
-                        c= c+1
-                    else:
+        if len(strs) > 1:
+            for i in strs:
+                if (len(i) < min_len):
+                    min_len = len(i)
+                    value = i
+                elif (len(i) == min_len):  
+                    value = i
+            #print("value: " + value)        
+            #print("min_len" + str(min_len))
+            main_str = [x for x in strs if x != value] 
+            #print(main_str)
+            #print("value of length : " + str(len(main_str)) )
+            if len(main_str) == 0:
+                output = strs[0]
+            if value != '':
+                for i in range(min_len):
+                    #print(i)
+                    c=0
+                    val = value[i]
+                    for x in main_str:
+                        if val in x[i]:
+                            c= c+1
+                        else:
+                            break
+                    #print("value of c : " + str(c) ) 
+                    if c == 0:
                         break
-                #print("value of c : " + str(c) )                      
-                if c == len(main_str):            
-                    output = output + value[i]            
+                    if c == len(main_str):            
+                        output = output + value[i]            
         return output
            
